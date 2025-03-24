93b29e8cb988d71b63b4d727137ba36bfe7108f9
rajeev <rajivg216@gmail.com>
2025-03-24 15:39:25 +0530

    User CRUD Done

---
+++ Backend/controller/UserController.js
@@ -0,0 +1,74 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// Admin only
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) {
+      return res.status(404).json({ message: "Users not found!" });
+    }
+    res.status(201).json({ message: "List of Users are :", users });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+// Admin or User Profile when Logged In.
+exports.getMyProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getUser = await User.findById(id);
+    if (!getUser) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    res.status(201).json({ mesaage: "The User's profile is :", getUser });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+// Admin or User Data Update.
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = { ...req.body };
+
+    // If password is being updated, hash it
+    if (updateData.password) {
+      const salt = await bcrypt.genSalt(10);
+      updateData.password = await bcrypt.hash(updateData.password, salt);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    res
+      .status(201)
+      .json({ message: "User Updated Successfully...", updatedUser });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+// Admin only
+exports.deleteUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    res
+      .status(201)
+      .json({ message: "User Deleted Successfully...", deletedUser });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const { authMiddleware, adminMiddleware } = require('../middleware/AuthMiddleware');
+const userController = require('../controller/UserController');
 const router = express.Router();
 
+// Get All Users (Admin only)
+router.get('/getAllUsers',authMiddleware, adminMiddleware, userController.getAllUsers);
+
+// Get Users by Id (Logged-In User)
+router.get('/getUserProfile/:id',authMiddleware, userController.getMyProfile);
+
+// Update User's profile (Admin or User)
+router.put('/updateUserProfile/:id',authMiddleware, userController.updateUserProfile);
+
+// Delete User (Admin only)
+router.delete('/deleteUserById/:id',authMiddleware, adminMiddleware, userController.deleteUserById);
 
 module.exports = router;
