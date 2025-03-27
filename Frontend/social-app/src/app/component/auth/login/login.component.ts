7d4f39209ee955186bad0d20054310483a20c950
rajeev <rajivg216@gmail.com>
2025-03-27 11:17:36 +0530

    Validations is done in a login Page

--- Frontend/social-app/src/app/component/auth/login/login.component.ts
+++ Frontend/social-app/src/app/component/auth/login/login.component.ts
@@ -3,25 +3,32 @@ import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { User } from '../../../model/user';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule, RouterLink, FormsModule],
+  imports: [RouterModule, RouterLink, FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  
-  user : User = new User();
-  
-  constructor(private authService : AuthService, private router : Router){}
-  
+  user: User = new User();
+
+  constructor(private authService: AuthService, private router: Router) {}
+
   onLogin() {
-    this.authService.login(this.user).subscribe(( res : any) => {
-      alert('User Login-In Successful...');
-      localStorage.setItem('token', JSON.stringify(res.user));
-      this.router.navigateByUrl('app-user-dashboard');
+    this.authService.login(this.user).subscribe((res: any) => {
+      if (res.user.role === 'Admin') {
+        localStorage.setItem('User-data', JSON.stringify(res.user));
+        localStorage.setItem('token', JSON.stringify(res.token));
+        alert(`${res.user.username} login successful...`);
+        this.router.navigateByUrl('app-admin-dashboard');
+      } else if(res.user.role === 'User') {
+        localStorage.setItem('User-data', JSON.stringify(res.user));
+        localStorage.setItem('token', JSON.stringify(res.token));
+        alert(`${res.user.username} login successful...`);
+        this.router.navigateByUrl('app-user-dashboard');
+      }
     });
   }
-
 }
